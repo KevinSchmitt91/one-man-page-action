cde634a631ef604a6813165012c28b5a640bffc5
ksmt33 <kevin.schmitt@mni.thm.de>
2016-11-15 16:52:49 +0100

    init

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package KS_landing_page
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'one-man-company-action' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'one-man-company-action' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'one-man-company-action' ), 'one-man-company-action', '<a href="http://kevinschmitt.info" rel="designer">Kevin Schmitt</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
