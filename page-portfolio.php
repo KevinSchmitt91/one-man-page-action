e0e2e197540961007caef0acceb3036d4e7d1108
Kevin Schmitt <kevin.schmitt@mni.thm.de>
2016-11-16 17:00:31 +0100

    added portfolio.php

---
+++ page-portfolio.php
@@ -0,0 +1,171 @@
+<?php
+/*
+	Template Name: Portfolio
+*/
+
+get_header(); ?>
+
+<!-- Process Intro -->
+<section id="process-intro">
+  <div class="container">
+    <div class="content-wrapper">
+      <div class="content">
+          <div class="col-sm-3">
+            <h2>Meine Projekte</h2>
+            <div class="divider-primary"></div>
+            <h5>Hier sind einige meiner Arbeiten.
+Machen Sie sich ein Bild davon.</h5>
+          </div><!-- col -->
+          <div class="col-sm-6 col-sm-offset-3">
+            <p>
+              Here’s a small sample of the things I’ve worked on so far.
+Think your idea would fit this line-up? Don’t hestitate to get in touch.
+We can work together to explore your possibilities and craft an outstanding digital product.
+            </p>
+          </div><!-- col -->
+      </div><!-- content -->
+    </div><!-- content-wrapper -->
+  </div><!-- container -->
+</section><!-- process-intro -->
+
+<!-- Portfolio-Enty -->
+<section id="portfolio-entry">
+  <div class="container">
+    <div class="content-wrapper">
+      <div class="content">
+        <div class="col-sm-4">
+          <div class="divider-primary"></div>
+          <h3 class="project-header-primary">Das Projekt: <small class="project-header-second">Beispielprojekt</small></h3>
+          <p class="project-desc">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+sed diam nonumy eirmod tempor invidunt ut labore et dolore
+magna aliquyam erat, sed diam voluptua. At vero eos et
+accusam et justo duo dolores et ea rebum. Stet clita kasd
+gubergren, no sea takimata sanctus est Lorem ipsum dolor
+sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+sed diam nonumy eirmod tempor invidunt ut labore et dolore
+magna aliquyam erat, sed diam voluptua. At vero eos et accusam e
+t justo duo dolores et ea rebum. Stet clita kasd gubergren,
+no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+          <div class="project-category">
+            <h4><small>Kategorie</small></h4>
+            <h5 class="project-category-header">Design und Entwicklung</h5>
+            <h4><small>Datum</small></h4>
+            <h5 class="project-category-header">2016</h5>
+          </div><!-- project-category -->
+        </div><!-- col -->
+        <div class="col-sm-6 col-sm-offset-2">
+          <img class="project-img" src="http://placehold.it/500x500" alt="placeholder" />
+        </div><!-- col -->
+      </div><!-- content -->
+    </div><!-- content-wrapper -->
+  </div><!-- container -->
+</section><!-- portfolio-entry -->
+
+<!-- Portfolio-Enty -->
+<section id="portfolio-entry">
+  <div class="container">
+    <div class="content-wrapper">
+      <div class="content">
+        <div class="col-sm-4">
+          <div class="divider-primary"></div>
+          <h3 class="project-header-primary">Das Projekt: <small class="project-header-second">Beispielprojekt</small></h3>
+          <p class="project-desc">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+sed diam nonumy eirmod tempor invidunt ut labore et dolore
+magna aliquyam erat, sed diam voluptua. At vero eos et
+accusam et justo duo dolores et ea rebum. Stet clita kasd
+gubergren, no sea takimata sanctus est Lorem ipsum dolor
+sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+sed diam nonumy eirmod tempor invidunt ut labore et dolore
+magna aliquyam erat, sed diam voluptua. At vero eos et accusam e
+t justo duo dolores et ea rebum. Stet clita kasd gubergren,
+no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+          <div class="project-category">
+            <h4><small>Kategorie</small></h4>
+            <h5 class="project-category-header">Design und Entwicklung</h5>
+            <h4><small>Datum</small></h4>
+            <h5 class="project-category-header">2016</h5>
+          </div><!-- project-category -->
+        </div><!-- col -->
+        <div class="col-sm-6 col-sm-offset-2">
+          <img class="project-img" src="http://placehold.it/500x500" alt="placeholder" />
+        </div><!-- col -->
+      </div><!-- content -->
+    </div><!-- content-wrapper -->
+  </div><!-- container -->
+</section><!-- portfolio-entry -->
+
+<!-- Portfolio-Enty -->
+<section id="portfolio-entry">
+  <div class="container">
+    <div class="content-wrapper">
+      <div class="content">
+        <div class="col-sm-4">
+          <div class="divider-primary"></div>
+          <h3 class="project-header-primary">Das Projekt: <small class="project-header-second">Beispielprojekt</small></h3>
+          <p class="project-desc">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+sed diam nonumy eirmod tempor invidunt ut labore et dolore
+magna aliquyam erat, sed diam voluptua. At vero eos et
+accusam et justo duo dolores et ea rebum. Stet clita kasd
+gubergren, no sea takimata sanctus est Lorem ipsum dolor
+sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+sed diam nonumy eirmod tempor invidunt ut labore et dolore
+magna aliquyam erat, sed diam voluptua. At vero eos et accusam e
+t justo duo dolores et ea rebum. Stet clita kasd gubergren,
+no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+          <div class="project-category">
+            <h4><small>Kategorie</small></h4>
+            <h5 class="project-category-header">Design und Entwicklung</h5>
+            <h4><small>Datum</small></h4>
+            <h5 class="project-category-header">2016</h5>
+          </div><!-- project-category -->
+        </div><!-- col -->
+        <div class="col-sm-6 col-sm-offset-2">
+          <img class="project-img" src="http://placehold.it/500x500" alt="placeholder" />
+        </div><!-- col -->
+      </div><!-- content -->
+    </div><!-- content-wrapper -->
+  </div><!-- container -->
+</section><!-- portfolio-entry -->
+
+<!-- Portfolio-Enty -->
+<section id="portfolio-entry">
+  <div class="container">
+    <div class="content-wrapper">
+      <div class="content">
+        <div class="col-sm-4">
+          <div class="divider-primary"></div>
+          <h3 class="project-header-primary">Das Projekt: <small class="project-header-second">Beispielprojekt</small></h3>
+          <p class="project-desc">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+sed diam nonumy eirmod tempor invidunt ut labore et dolore
+magna aliquyam erat, sed diam voluptua. At vero eos et
+accusam et justo duo dolores et ea rebum. Stet clita kasd
+gubergren, no sea takimata sanctus est Lorem ipsum dolor
+sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+sed diam nonumy eirmod tempor invidunt ut labore et dolore
+magna aliquyam erat, sed diam voluptua. At vero eos et accusam e
+t justo duo dolores et ea rebum. Stet clita kasd gubergren,
+no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+          <div class="project-category">
+            <h4><small>Kategorie</small></h4>
+            <h5 class="project-category-header">Design und Entwicklung</h5>
+            <h4><small>Datum</small></h4>
+            <h5 class="project-category-header">2016</h5>
+          </div><!-- project-category -->
+        </div><!-- col -->
+        <div class="col-sm-6 col-sm-offset-2">
+          <img class="project-img" src="http://placehold.it/500x500" alt="placeholder" />
+        </div><!-- col -->
+      </div><!-- content -->
+    </div><!-- content-wrapper -->
+  </div><!-- container -->
+</section><!-- portfolio-entry -->
+
+<?php get_footer();?>
