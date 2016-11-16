662798c4c01ef028f496e2a52475449182700c78
Kevin Schmitt <kevin.schmitt@mni.thm.de>
2016-11-16 16:56:36 +0100

    added prozess.php

---
+++ page-prozess.php
@@ -0,0 +1,120 @@
+<?php
+/*
+	Template Name: Prozess
+*/
+
+get_header(); ?>
+
+<!-- Process Intro -->
+<section id="process-intro">
+  <div class="container">
+    <div class="content-wrapper">
+      <div class="content">
+          <div class="col-sm-3">
+            <h2>Der Prozess</h2>
+            <div class="divider-primary"></div>
+            <h5>Zusammen können wir einzigartige
+Projekte erstellen.</h5>
+          </div><!-- col -->
+          <div class="col-sm-6 col-sm-offset-3">
+            <p>
+              Through an innovative, six stage process, we’ll make sure your product is as good
+as it gets. The design and build process will help us research, explore and finely craft
+your product, covering everything from user experience through to final visual design
+and branding, including all assets, interactions and motions.
+            </p>
+          </div><!-- col -->
+      </div><!-- content -->
+    </div><!-- content-wrapper -->
+  </div><!-- container -->
+</section><!-- process-intro -->
+
+<!-- Process-Part -->
+<section id="process-complete">
+  <div class="process-part" style="background-color: #01B5AF">
+    <div class="container">
+      <div class="content-wrapper">
+        <div class="content">
+          <div class="col-sm-3">
+            <h2>001</h2>
+            <h2>Planen</h2>
+          </div><!-- col -->
+          <div class="col-sm-6 col-sm-offset-3">
+            <p>
+              For me, and the people I work with, good research is
+fundamental to any project. We’ll dive into product’s domain.
+Generate knowledge about the target audience and their
+needs, find what value we can bring them. We research business
+goals and we map out the market and any possible competitors.
+            </p>
+          </div><!-- col -->
+        </div><!-- content -->
+      </div><!-- content-wrapper -->
+    </div><!-- container -->
+  </div><!-- process-part -->
+  <div class="process-part" style="background-color: #4A4A4A">
+    <div class="container">
+      <div class="content-wrapper">
+        <div class="content">
+          <div class="col-sm-3">
+            <h2>001</h2>
+            <h2>Planen</h2>
+          </div><!-- col -->
+          <div class="col-sm-6 col-sm-offset-3">
+            <p>
+              For me, and the people I work with, good research is
+fundamental to any project. We’ll dive into product’s domain.
+Generate knowledge about the target audience and their
+needs, find what value we can bring them. We research business
+goals and we map out the market and any possible competitors.
+            </p>
+          </div><!-- col -->
+        </div><!-- content -->
+      </div><!-- content-wrapper -->
+    </div><!-- container -->
+  </div><!-- process-part -->
+  <div class="process-part secondary-font">
+    <div class="container">
+      <div class="content-wrapper">
+        <div class="content">
+          <div class="col-sm-3">
+            <h2>001</h2>
+            <h2>Planen</h2>
+          </div><!-- col -->
+          <div class="col-sm-7 col-sm-offset-3">
+            <p>
+              For me, and the people I work with, good research is
+fundamental to any project. We’ll dive into product’s domain.
+Generate knowledge about the target audience and their
+needs, find what value we can bring them. We research business
+goals and we map out the market and any possible competitors.
+            </p>
+          </div><!-- col -->
+        </div><!-- content -->
+      </div><!-- content-wrapper -->
+    </div><!-- container -->
+  </div><!-- process-part -->
+  <div class="process-part" style="background-color: #01B5AF">
+    <div class="container">
+      <div class="content-wrapper">
+        <div class="content">
+          <div class="col-sm-3">
+            <h2>001</h2>
+            <h2>Planen</h2>
+          </div><!-- col -->
+          <div class="col-sm-6 col-sm-offset-3">
+            <p>
+              For me, and the people I work with, good research is
+fundamental to any project. We’ll dive into product’s domain.
+Generate knowledge about the target audience and their
+needs, find what value we can bring them. We research business
+goals and we map out the market and any possible competitors.
+            </p>
+          </div><!-- col -->
+        </div><!-- content -->
+      </div><!-- content-wrapper -->
+    </div><!-- container -->
+  </div><!-- process-part -->
+</section><!--process-complete -->
+
+<?php get_footer();?>
