c845e83c0dfc0881cffd854c6e6d4d9696237aae
Kevin Schmitt <kevin.schmitt@mni.thm.de>
2016-11-17 13:17:26 +0100

    updatet portfolio

--- page-portfolio.php
+++ page-portfolio.php
@@ -13,14 +13,15 @@ get_header(); ?>
           <div class="col-sm-3">
             <h2>Meine Projekte</h2>
             <div class="divider-primary"></div>
-            <h5>Hier sind einige meiner Arbeiten.
+            <h5>Hier sind einige unserer Arbeiten.
 Machen Sie sich ein Bild davon.</h5>
           </div><!-- col -->
           <div class="col-sm-6 col-sm-offset-3">
             <p>
-              Here’s a small sample of the things I’ve worked on so far.
-Think your idea would fit this line-up? Don’t hestitate to get in touch.
-We can work together to explore your possibilities and craft an outstanding digital product.
+              Hier gibts es einige Beispiele von Arbeiten die wir bisher abgeschlossen haben.
+              Denken Sie Ihre Idee wurde hier reinpassen?
+              Schreiben Sie einfach eine E-Mail oder nutzen Sie das Kontaktformular.
+              Wir können mit Ihnen zusammenarbeiten um ein einzigartiges Produkt zu erschaffen.
             </p>
           </div><!-- col -->
       </div><!-- content -->
@@ -35,28 +36,26 @@ We can work together to explore your possibilities and craft an outstanding digi
       <div class="content">
         <div class="col-sm-4">
           <div class="divider-primary"></div>
-          <h3 class="project-header-primary">Das Projekt: <small class="project-header-second">Beispielprojekt</small></h3>
+          <h3 class="project-header-primary">Das Projekt: <small class="project-header-second">Freiwillige Feuerwehr Albach</small></h3>
           <p class="project-desc">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-sed diam nonumy eirmod tempor invidunt ut labore et dolore
-magna aliquyam erat, sed diam voluptua. At vero eos et
-accusam et justo duo dolores et ea rebum. Stet clita kasd
-gubergren, no sea takimata sanctus est Lorem ipsum dolor
-sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-sed diam nonumy eirmod tempor invidunt ut labore et dolore
-magna aliquyam erat, sed diam voluptua. At vero eos et accusam e
-t justo duo dolores et ea rebum. Stet clita kasd gubergren,
-no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            Die Freiwillige Feuerwehr Fernwald-Albach in der wir auch tätig sind, hatte uns gebeten eine
+            Modernisierung der in die jahre gekommenen Webseite durchzuführen.
+            <br>
+            Aus diesem Auftrag heraus wurde mit unserem Prozess-Verfahren eine neue, moderne, für Mobilgeräte
+            angepasste Webseite entwickelt.
+            <br>
+            Diese ist 2015 online gegangen.
           </p>
+          <a href="http://feuerwehr-albach.de" target="_blank">www.feuerwehr-albach.de</a>
           <div class="project-category">
             <h4><small>Kategorie</small></h4>
-            <h5 class="project-category-header">Design und Entwicklung</h5>
+            <h5 class="project-category-header">Webdesign und Webentwicklung</h5>
             <h4><small>Datum</small></h4>
-            <h5 class="project-category-header">2016</h5>
+            <h5 class="project-category-header">2015</h5>
           </div><!-- project-category -->
         </div><!-- col -->
         <div class="col-sm-6 col-sm-offset-2">
-          <img class="project-img" src="http://placehold.it/500x500" alt="placeholder" />
+          <img class="project-img" src="/kevinschmitt/wp-content/uploads/2016/11/Logo_Feuerwehr_Albach.png" alt="Feuerwehr Albach Logo" />
         </div><!-- col -->
       </div><!-- content -->
     </div><!-- content-wrapper -->
@@ -70,28 +69,25 @@ no sea takimata sanctus est Lorem ipsum dolor sit amet.
       <div class="content">
         <div class="col-sm-4">
           <div class="divider-primary"></div>
-          <h3 class="project-header-primary">Das Projekt: <small class="project-header-second">Beispielprojekt</small></h3>
+          <h3 class="project-header-primary">Das Projekt: <small class="project-header-second">MF-Blaukat GbR</small></h3>
           <p class="project-desc">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-sed diam nonumy eirmod tempor invidunt ut labore et dolore
-magna aliquyam erat, sed diam voluptua. At vero eos et
-accusam et justo duo dolores et ea rebum. Stet clita kasd
-gubergren, no sea takimata sanctus est Lorem ipsum dolor
-sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-sed diam nonumy eirmod tempor invidunt ut labore et dolore
-magna aliquyam erat, sed diam voluptua. At vero eos et accusam e
-t justo duo dolores et ea rebum. Stet clita kasd gubergren,
-no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            Die MF-Blaukat GbR würde Ende 2015 als kleines Bauunternehmen im Estrich-Gewerbe gegründet.
+            <br>
+            Das wichtigste für eine neue Firma oder Startup ist nicht nur eine moderne Webseite, sondern
+            auch ein aussagekräftiges Logo.
+            <br>
+            Wir entwurfen und entwickelten ein modernes und aussagekräftiges Logo mit Wiedererkennungswert.
           </p>
+          <a href="http://mf-blaukat.de">www.mf-blaukat.de</a>
           <div class="project-category">
             <h4><small>Kategorie</small></h4>
-            <h5 class="project-category-header">Design und Entwicklung</h5>
+            <h5 class="project-category-header">Logodesign</h5>
             <h4><small>Datum</small></h4>
-            <h5 class="project-category-header">2016</h5>
+            <h5 class="project-category-header">2015</h5>
           </div><!-- project-category -->
         </div><!-- col -->
         <div class="col-sm-6 col-sm-offset-2">
-          <img class="project-img" src="http://placehold.it/500x500" alt="placeholder" />
+          <img class="project-img" src="/kevinschmitt/wp-content/uploads/2016/11/Blaukat_logo.png" alt="MF-Blaukat Logo" />
         </div><!-- col -->
       </div><!-- content -->
     </div><!-- content-wrapper -->
@@ -105,63 +101,23 @@ no sea takimata sanctus est Lorem ipsum dolor sit amet.
       <div class="content">
         <div class="col-sm-4">
           <div class="divider-primary"></div>
-          <h3 class="project-header-primary">Das Projekt: <small class="project-header-second">Beispielprojekt</small></h3>
+          <h3 class="project-header-primary">Das Projekt: <small class="project-header-second">Hausschlachtung Zopf</small></h3>
           <p class="project-desc">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-sed diam nonumy eirmod tempor invidunt ut labore et dolore
-magna aliquyam erat, sed diam voluptua. At vero eos et
-accusam et justo duo dolores et ea rebum. Stet clita kasd
-gubergren, no sea takimata sanctus est Lorem ipsum dolor
-sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-sed diam nonumy eirmod tempor invidunt ut labore et dolore
-magna aliquyam erat, sed diam voluptua. At vero eos et accusam e
-t justo duo dolores et ea rebum. Stet clita kasd gubergren,
-no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            Das Unternehmen Hausschlachtung Zopf wurde Anfang 2016 gegründet.
+            <br>
+            Auch hier wurde ein aussagekäftiges und wiederzuerkennendes Logo erwünscht.
+            <br>
+            Zusätzlich auch eine Webseite, die sich aktuell aber noch in Entwicklung befindet.
           </p>
           <div class="project-category">
             <h4><small>Kategorie</small></h4>
-            <h5 class="project-category-header">Design und Entwicklung</h5>
+            <h5 class="project-category-header">Logo- und Webdesign</h5>
             <h4><small>Datum</small></h4>
             <h5 class="project-category-header">2016</h5>
           </div><!-- project-category -->
         </div><!-- col -->
         <div class="col-sm-6 col-sm-offset-2">
-          <img class="project-img" src="http://placehold.it/500x500" alt="placeholder" />
-        </div><!-- col -->
-      </div><!-- content -->
-    </div><!-- content-wrapper -->
-  </div><!-- container -->
-</section><!-- portfolio-entry -->
-
-<!-- Portfolio-Enty -->
-<section id="portfolio-entry">
-  <div class="container">
-    <div class="content-wrapper">
-      <div class="content">
-        <div class="col-sm-4">
-          <div class="divider-primary"></div>
-          <h3 class="project-header-primary">Das Projekt: <small class="project-header-second">Beispielprojekt</small></h3>
-          <p class="project-desc">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-sed diam nonumy eirmod tempor invidunt ut labore et dolore
-magna aliquyam erat, sed diam voluptua. At vero eos et
-accusam et justo duo dolores et ea rebum. Stet clita kasd
-gubergren, no sea takimata sanctus est Lorem ipsum dolor
-sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-sed diam nonumy eirmod tempor invidunt ut labore et dolore
-magna aliquyam erat, sed diam voluptua. At vero eos et accusam e
-t justo duo dolores et ea rebum. Stet clita kasd gubergren,
-no sea takimata sanctus est Lorem ipsum dolor sit amet.
-          </p>
-          <div class="project-category">
-            <h4><small>Kategorie</small></h4>
-            <h5 class="project-category-header">Design und Entwicklung</h5>
-            <h4><small>Datum</small></h4>
-            <h5 class="project-category-header">2016</h5>
-          </div><!-- project-category -->
-        </div><!-- col -->
-        <div class="col-sm-6 col-sm-offset-2">
-          <img class="project-img" src="http://placehold.it/500x500" alt="placeholder" />
+          <img class="project-img" src="/kevinschmitt/wp-content/uploads/2016/11/Logo_Entwurf_1.png" alt="Hausschlachtung Zopf Logo" />
         </div><!-- col -->
       </div><!-- content -->
     </div><!-- content-wrapper -->
